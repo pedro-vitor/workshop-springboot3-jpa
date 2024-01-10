7ece1c7a00c9fac605ee4d2ed98dc40748d74813
Pedro Vitor <pedrocoud12@gmail.com>
2024-01-10 15:02:44 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -24,10 +24,7 @@ public class Order implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // formatar a
-																											// data no
-																											// padrão
-																											// UTC
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // formatar a data no padrão UTC
 	private Instant moment;
 
 	private Integer orderStatus;
